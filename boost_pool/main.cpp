6beb181f4a83bc1cb8f35b8098546e69791e2833
jorgonlor <jorgonlor@gmail.com>
2015-11-15 17:54:10 +0100

    Update main.cpp

--- boost_pool/main.cpp
+++ boost_pool/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <boost/pool/pool.hpp>
+#include <boost/pool/object_pool.hpp>
+
+struct A
+{
+    A() {}
+    ~A() {}
+    
+    int data;
+};
 
 int main(int argc, char **argv)
 {
@@ -16,10 +25,24 @@ int main(int argc, char **argv)
         
     end = std::chrono::system_clock::now();
     
-    // Measure time allocationg using c++ new (leaked on purpose, only measuring allocation)
     std::chrono::duration<double> elapsed_seconds = end-start;
     std::cout << "Pool time: " << elapsed_seconds.count() << std::endl;
     
+    // Measure time allocationg with boost::pool
+    // Object pool will call all A destructors when pool is destructed
+    // (but it does not call constructors when allocating)
+    start = std::chrono::system_clock::now();
+    
+    boost::object_pool<A> op;
+    for(int i = 0; i < numIters; ++i)
+        A* const pi = static_cast<A*>(op.malloc());
+        
+    end = std::chrono::system_clock::now();
+    
+    elapsed_seconds = end-start;
+    std::cout << "Object pool time: " << elapsed_seconds.count() << std::endl;
+    
+    // Measure time allocationg using c++ new (leaked on purpose, only measuring allocation)
     start = std::chrono::system_clock::now();
     
     for(int i = 0; i < numIters; ++i)
@@ -35,4 +58,5 @@ int main(int argc, char **argv)
 
 // Obtained output:
 // Pool time: 0.14
+// Object pool: 0.17
 // C++ new time: 0.22
